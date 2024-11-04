a32d419c0dfdbc556a5afa905e017f880fee9945
Jared Edwards <jared@konstruct.io>
2024-11-04 11:42:33 -0700

    fix v for w

--- cmd/destroy.go
+++ cmd/destroy.go
@@ -22,23 +22,24 @@ func getDestroyCommand() *cobra.Command {
 			log.Info("creating docker client")
 			dockerCLI, err := docker.New(log)
 			if err != nil {
-				return fmt.Errorf("error creating docker client: %v ", err)
+				return fmt.Errorf("error creating docker client: %w ", err)
 			}
 
 			err = dockerCLI.RemoveColonyK3sContainer(ctx)
 			if err != nil {
-				return fmt.Errorf("error: %v", err)
+				return fmt.Errorf("error: failed to remove colony container %w ", err)
 			}
 
 			if fileExists(constants.KubeconfigHostPath) {
 				err := os.Remove(constants.KubeconfigHostPath)
 				if err != nil {
-					return fmt.Errorf("error removing file: %v ", err)
+					return fmt.Errorf("error removing kubeconfig: %w ", err)
 				}
 				log.Info("kubeconfig deleted")
 			} else {
 				log.Info("kubeconfig does not exist, proceeding with remaining deletion")
 			}
+
 			log.Info("colony installation removed from host")
 
 			return nil
